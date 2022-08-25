2398b4d9ab61153715f5e6f06a28038fb10fa092
Sergey Goncharovр <doimpos@gmail.com>
2022-08-26 03:45:35 +0500

    Доработки главной

---
+++ src/blocks/modules/first_block/first_block.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+import 'slick-carousel';
+
+const FirstBlock = class FirstBlock {
+    constructor() {}
+    slickFirst() {
+        $('.js_slider_first').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: false,
+            arrows: true,
+            adaptiveHeight: true,
+            dots: true
+        });
+    }
+    init() {
+        this.slickFirst();
+    }
+}
+
+export default FirstBlock;
--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,12 @@
 import * as globalFunctions from './modules/functions.js';
 globalFunctions.isWebp();
 
-
 import $ from 'jquery';
 
+import FirstBlock from '../blocks/modules/first_block/first_block.js';
+
 $(function () {
-    
+    const firstBlock = new FirstBlock();
+
+    firstBlock.init();
 })
