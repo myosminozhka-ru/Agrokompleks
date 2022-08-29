2b0134f98ade8ac1c33c9948b3a5637999cc09a5
Sergey Goncharovр <doimpos@gmail.com>
2022-08-30 01:14:19 +0500

    Доработки главной

--- src/blocks/modules/block_terms/block_terms.js
+++ src/blocks/modules/block_terms/block_terms.js
@@ -4,30 +4,33 @@ import 'slick-carousel';
 const BlockTerms = class BlockTerms {
     constructor() {}
     slickTerms() {
-        // if ( window.matchMedia('(max-width : 1023px)').matches ) {
-        //     $('.js_slider_spec').slick({
-        //         slidesToShow: 3,
-        //         slidesToScroll: 1,
-        //         infinite: false,
-        //         arrows: true,
-        //         dots: true,
-        //         responsive: [
-        //           {
-        //             breakpoint: 750,
-        //             settings: {
-        //               slidesToShow: 2
-        //             }
-        //           },
-        //         ]
-        //     });
-        // }
+        if ( window.matchMedia('(max-width : 1023px)').matches ) {
+            $('.sl_js_for').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                fade: true,
+                asNavFor: '.sl_js_nav'
+              });
+              $('.sl_js_nav').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                asNavFor: '.sl_js_for',
+                dots: true,
+                // centerMode: true,
+                // focusOnSelect: true,
+                variableWidth: true
+              });
+        }
     }
     tabsTerms() {
-        $('.tabs__contents').on('click', '.tabs__content:not(.active)', function() {
-            $(this)
-              .addClass('active').siblings().removeClass('active')
-              .closest('.tabs').find('.tabs__caption li').removeClass('active').hide(500).eq($(this).index()).addClass('active').show(500);
-        });
+        if ( window.matchMedia('(min-width : 1024px)').matches ) {
+            $('.tabs__contents').on('click', '.tabs__content:not(.active)', function() {
+                $(this)
+                .addClass('active').siblings().removeClass('active')
+                .closest('.tabs').find('.tabs__caption li').removeClass('active').hide(500).eq($(this).index()).addClass('active').show(500);
+            });
+        };
     }
     init() {
         this.slickTerms();
